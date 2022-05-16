4b4191863d93fc3f3f6c11e766afeda476ca93f8
Ugur Bozkurt <94750332+UgurBzkrt@users.noreply.github.com>
2022-05-16 23:46:48 +0300

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Java the second");
+        System.out.println("Hello from Java");
     }
 }
 
